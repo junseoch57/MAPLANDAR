231dd635e195e50f0dbb6833992dbaf8008e58c1
jade2 <jade2180@naver.com>
2025-05-23 16:57:04 +0900

    그룹 도메인 추가

---
+++ src/main/java/com/example/maplander_be/domain/ListOfGroup.java
@@ -0,0 +1,52 @@
+package com.example.maplander_be.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ListOfGroup")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+
+public class ListOfGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_id")
+    private Integer groupId;
+
+    @Column(name = "group_name", nullable = false, length = 100)
+    private String groupName;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GroupMember> members = new ArrayList<>();
+
+
+    public static ListOfGroup create(User owner, String groupName, List<User> initialMembers) {
+        ListOfGroup group = ListOfGroup.builder()
+                .owner(owner)
+                .groupName(groupName)
+                .build();
+
+        group.members.add(GroupMember.of(group, owner, Role.OWNER));
+        initialMembers.forEach(u ->
+                group.members.add(GroupMember.of(group, u, Role.MEMBER))
+        );
+        return group;
+    }
+
+}
